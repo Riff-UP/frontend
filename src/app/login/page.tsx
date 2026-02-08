df8c5739d3119fe5a8ef3f30937972cf52c674ff
Diego-Zarate18 <diegoazarate110800@gmail.com>
2026-02-07 22:31:19 -0600

    Creation of views: login and register

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import AuthLayout from "../components/AuthLayout";
+import Login from "../components/Login";
+
+export default function LoginPage() {
+  return (
+    <AuthLayout>
+      <Login />
+    </AuthLayout>
+  );
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,10 @@
+import AuthLayout from "../components/AuthLayout";
+import Register from "../components/Register";
+
+export default function RegisterPage() {
+  return (
+    <AuthLayout>
+      <Register />
+    </AuthLayout>
+  );
+}
